da64b9f0619fa13ad8d6cf965955d0e358ec836f
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-11-15 09:55:42 +0100

    used the shared module as a means to make available other modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,18 +1,45 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {
+  NgbCarousel, NgbCollapse,
+  NgbDropdown,
+  NgbDropdownMenu, NgbDropdownToggle,
+  NgbNav,
+  NgbNavContent,
+  NgbNavItem,
+  NgbNavLink,
+  NgbNavOutlet, NgbSlide
+} from "@ng-bootstrap/ng-bootstrap";
+import {RouterLink} from "@angular/router";
+import { MenuComponent } from './menu/menu.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    MenuComponent
   ],
   exports: [
     HeaderComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    NgbNavContent,
+    NgbNavItem,
+    NgbNav,
+    NgbNavLink,
+    NgbNavOutlet,
+    NgbDropdown,
+    NgbDropdownMenu,
+    NgbDropdownToggle,
+    RouterLink,
+    NgbCarousel,
+    NgbSlide,
+    NgbCollapse
   ]
 })
 export class SharedModule { }
