d7b51b8b16326df3d8ad9a646249e512305b4711
Chukwudi Ike-Nwako <ikenwakochukwudi@gmail.com>
2023-11-15 09:51:43 +0100

    modified the app module and the routing module also

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
-import {SharedModule} from "./shared/shared.module";
+import { SharedModule } from './shared/shared.module';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { NgbRating } from '@ng-bootstrap/ng-bootstrap';
+import { LightboxModule } from 'ngx-lightbox';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        SharedModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    SharedModule,
+    SlickCarouselModule,
+    NgbRating,
+    LightboxModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
